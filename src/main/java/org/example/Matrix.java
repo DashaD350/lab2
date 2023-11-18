85aa2ab5577776c062b5cace2e651d8f30eb293d
DashaD350 <dashaevora@gmail.com>
2023-11-18 08:31:44 +0200

    крок2

--- src/main/java/org/example/Matrix.java
+++ src/main/java/org/example/Matrix.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Random;
+
 public class Matrix {
 
     private int rows;
@@ -7,5 +9,33 @@ public class Matrix {
     private float[][] data;
 
     public Matrix(){}
+
+    // Конструктор для створення порожньої матриці
+    public void emptyMatrix(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.data = new float[rows][columns];
+    }
+
+    // Конструктор для створення копії іншої матриці
+    public void copyMatrix (Matrix other) {
+        this.rows = other.rows;
+        this.columns = other.columns;
+        this.data = new float[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            System.arraycopy(other.data[i], 0, this.data[i], 0, columns);
+        }
+    }
+
+    //Метод для стоворення рандомної матриці
+    public void random(int rows, int columns){
+        this.data = new float[rows][columns];
+        Random random = new Random();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                data[i][j] = random.nextFloat();;
+            }
+        }
+    }
 
 }
