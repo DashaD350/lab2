9f5ed1fb1fa2aff62456576f3f8c939845b42e99
DashaD350 <dashaevora@gmail.com>
2023-11-18 08:25:24 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Matrix mat = new Matrix();
+        Matrix mat1 = new Matrix();
+        Matrix mat2 = new Matrix();
+        Matrix mat3 = new Matrix();
+
+    }
+}
---
+++ src/main/java/org/example/Matrix.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Matrix {
+
+    private int rows;
+    private int columns;
+    private float[][] data;
+
+    public Matrix(){}
+
+}
